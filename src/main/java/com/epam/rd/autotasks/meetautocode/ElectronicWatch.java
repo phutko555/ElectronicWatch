3984a3f0de804a34a84fb3aac4fadbaca26bd50d
Giorgi Phutkaradze <phutkaradzeg3@gmail.com>
2024-01-10 18:11:53 +0000

    Update ElectronicWatch.java

--- src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
+++ src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
@@ -5,8 +5,13 @@ import java.util.Scanner;
 public class ElectronicWatch {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Input: ");
         int seconds = scanner.nextInt();
+        int hours = seconds / 3600;
+        int minutes = (seconds % 3600) / 60;
+        seconds = seconds % 60;
 
+        System.out.printf("Output: "+"%d:%02d:%02d%n", hours, minutes, seconds);
 
     }
 }
